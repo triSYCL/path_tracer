f5f6b8521a34d47bd899d80cfba3f953e414bdeb
Ronan Keryell <rkeryell@xilinx.com>
2020-09-30 15:42:26 -0700

    Use default constructor to simplify

--- RT_SYCL/ray.h
+++ RT_SYCL/ray.h
@@ -4,25 +4,27 @@
 #include "vec3.h"
 
 class ray {
-public:
-    ray() { }
-    ray(const point3& origin, const vec3& direction)
-        : orig(origin)
-        , dir(direction)
-    {
-    }
-
-    point3 origin() const { return orig; }
-    vec3 direction() const { return dir; }
-
-    point3 at(double t) const
-    {
-        return orig + t * dir;
-    }
 
 public:
-    point3 orig;
-    vec3 dir;
+
+  ray() = default;
+
+  ray(const point3& origin, const vec3& direction)
+    : orig(origin)
+    , dir(direction) { }
+
+  point3 origin() const { return orig; }
+
+  vec3 direction() const { return dir; }
+
+  point3 at(double t) const {
+    return orig + t*dir;
+  }
+
+public:
+
+  point3 orig;
+  vec3 dir;
 };
 
 #endif
