310d0b578dc2787ac69cf14a2f8879890ada9e59
Ronan Keryell <rkeryell@xilinx.com>
2020-09-30 15:34:27 -0700

    Simplify the camera
    C++ should be as simple as Python

--- RT_SYCL/camera.h
+++ RT_SYCL/camera.h
@@ -4,29 +4,26 @@
 #include "rtweekend.h"
 
 class camera {
+
 public:
-    camera()
-    {
-        auto aspect_ratio = 16.0 / 9.0;
-        auto viewport_height = 2.0;
-        auto viewport_width = aspect_ratio * viewport_height;
-        auto focal_length = 1.0;
 
-        origin = point3(0, 0, 0);
-        horizontal = vec3(viewport_width, 0.0, 0.0);
-        vertical = vec3(0.0, viewport_height, 0.0);
-        lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
-    }
+  static auto constexpr aspect_ratio = 16.0/9.0;
+  static auto constexpr viewport_height = 2.0;
+  static auto constexpr viewport_width = aspect_ratio*viewport_height;
+  static auto constexpr focal_length = 1.0;
 
-    ray get_ray(double u, double v) const
-    {
-        return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-    }
+  ray get_ray(double u, double v) const {
+    return
+      { origin, lower_left_corner + u * horizontal + v * vertical - origin };
+  }
 
 private:
-    point3 origin;
-    point3 lower_left_corner;
-    vec3 horizontal;
-    vec3 vertical;
+
+  point3 origin;
+  point3 lower_left_corner =
+    origin - horizontal/2 - vertical/2 - vec3 { 0, 0, focal_length };
+  vec3 horizontal { viewport_width, 0.0, 0.0 };
+  vec3 vertical { 0.0, viewport_height, 0.0 };
+
 };
 #endif
