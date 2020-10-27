1c7b4c8314f310e27a881a33db966e55843c1c3c
Krishna Kumar Ranipet Murugan <kranipet@xsjsycl40.xilinx.com>
2020-10-27 16:43:32 -0700

    removed unused variables

--- RT_SYCL/hitable.hpp
+++ RT_SYCL/hitable.hpp
@@ -6,24 +6,13 @@
 #include "texture.hpp"
 #include "vec3.hpp"
 
-enum class material_t { Lambertian,
-    Metal,
-    Dielectric };
-
 class hit_record {
 public:
-    
-    vec3 center;
-    real_t radius;
-
     double t;
     point3 p;
     vec3 normal;
     double u;
     double v;
-
-    // material properties
-    //Material_t material_type;
 };
 
 #endif
