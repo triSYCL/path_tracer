651badc4f07f776c660a8cff59db74a485f349f2
Krishna Kumar Ranipet Murugan <kranipet@xsjsycl40.xilinx.com>
2020-10-27 13:47:19 -0700

    Update material.hpp

--- RT_SYCL/material.hpp
+++ RT_SYCL/material.hpp
@@ -1,37 +1,40 @@
 #ifndef RT_SYCL_MATERIAL_HPP
 #define RT_SYCL_MATERIAL_HPP
-#include "hittable.hpp"
+#include "hitable.hpp"
 #include "vec3.hpp"
 #include <iostream>
 #include <variant>
 #include <vector>
 
 struct lambertian_material {
-    lambertian() = default;
-    lambertian(const color& a)
-        : albedo { a }
+    lambertian_material() = default;
+    lambertian_material(const color& a)
+        : albedo { solid_texture{ a } }
     {
     }
+    lambertian_material( texture_t& a)
+        :albedo { a }
+    {}
 
-    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, rat& scattered) const
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const
     {
         vec3 scatter_direction = rec.normal + random_unit_vector();
         scattered = ray(rec.p, scatter_direction);
-        attenuation = albedo;
+        attenuation = std::visit([&](auto&& arg) { return arg.value(rec.u, rec.v, rec.p); },albedo);
         return true;
     }
-    color albedo;
+    texture_t albedo;
 };
 
 struct metal_material {
-    metal() = default;
-    metal(const color& a, double f)
+    metal_material() = default;
+    metal_material(const color& a, double f)
         : albedo { a }
         , fuzz { std::clamp(f, 0.0, 1.0) }
     {
     }
 
-    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, rat& scattered) const
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const
     {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
         scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
@@ -44,9 +47,9 @@ struct metal_material {
 
 struct dielectric_material{
     dielectric_material() = default;
-    dielectric(double ri) : ref_idx {ri}{}
+    dielectric_material(double ri) : ref_idx {ri}{}
 
-    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, rat& scattered) const
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const
     {
         attenuation = color{0.1,0.1,0.1};
         double etai_over_etat = ref_idx;
@@ -58,6 +61,6 @@ struct dielectric_material{
     double ref_idx;
 };
 
-//using material_t = std::variant<lambertian_material, metal_material, dielectric_material>;
+using Material_t = std::variant<lambertian_material, metal_material, dielectric_material>;
 
 #endif
