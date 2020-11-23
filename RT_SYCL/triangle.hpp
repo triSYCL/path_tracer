0fba2cea1337d1876c26034b20988c09e86988c6
Krishna Kumar Ranipet Murugan <kranipet@xsjsycl40.xilinx.com>
2020-11-23 13:55:53 -0800

    update triangle.hpp

--- RT_SYCL/triangle.hpp
+++ RT_SYCL/triangle.hpp
@@ -11,13 +11,10 @@
 class triangle{
 public:
     triangle() = default;
-    triangle(real_t _x0, real_t _y0, real_t _x1, real_t _y1, real_t _x2, real_t _y2, material_t mat_type)
-    : x0 { _x0 }
-    , x1 { _x1 }
-    , x2 { _x2 }
-    , y0 { _y0 }
-    , y1 { _y1 }
-    , y2 { _y2 }
+    triangle(point _v0, point _v1, point _v2, material_t mat_type)
+    : v0 { _v0 }
+    , v1 { _v1 }
+    , v2 { _v2 }
     , material_type { mat_type }
     {}
 
@@ -25,10 +22,45 @@ public:
     {
         hit_material_type = material_type;
 
+        // Get triangle edge vectors and plae normal
+        auto u = v1-v0;
+        auto v = v2-v0;
+        vec outward_normal = sycl::cross(u, v);
+        rec.set_face_normal(r,outward_normal);
+
+        auto w0 = r.origin() - v0;
+        auto a = -sycl::dot(outward_normal,w0);
+        auto b = sycl::dot(outward_normal,r.direction());
+        if (sycl::fabs(b) < 0.000001) return false; // ray is parallel to triangle plane
+
+        // intersection point of ray with traingle
+        real_t length = a/b;
+        if(length<0) return false;
+        else if (length < min || length > max)
+            return false;
+
+        vec hit_pt = r.at(rec.t);
+        auto uu = sycl::dot(u, u);
+        auto uv = sycl::dot(u, v);
+        auto vv = sycl::dot(v, v);
+        auto  w = hit_pt - v0;
+        auto wu = sycl::dot(w, u);
+        auto wv = sycl::dot(w, v);
+        auto D = uv * uv - uu * vv;
+
+        auto s = (uv * wv - vv * wu) / D;
+        auto t = (uv * wu - uu * wv) / D;
+        if(s < 0.0 || s > 1.0 || t < 0.0 || (s+t) > 1.0)
+            return false;
+
+        rec.set_face_normal(r,outward_normal);
+        rec.t = length;
+        rec.p = hit_pt;
+        return true;
 
     }
 
-    real_t x0,x1,x2,y0,y1,y2;
+    point v0,v1,v2;
     material_t material_type;
 };
 #endif
