be688056e45625f6e5ef501aa5c576313d6e6415
Luc Forget <luc.forget@insa-lyon.fr>
2021-02-26 15:34:48 +0100

    Remove pointers in render lambda + force no-exceptions for sycl compiler

--- include/build_parameters.hpp
+++ include/build_parameters.hpp
@@ -2,7 +2,7 @@
 #define BUILD_PARAMETERS_HPP
 namespace buildparams {
 
-#if USE_SINGLE_TASK
+#ifdef USE_SINGLE_TASK
 constexpr bool use_single_task = true;
 #else
 constexpr bool use_single_task = false;
--- include/xorshift.hpp
+++ include/xorshift.hpp
@@ -5,8 +5,7 @@
 #include <cstdint>
 #include <limits>
 
-template <int Nbits = 32>
-struct xorshift {
+template <int Nbits = 32> struct xorshift {
   /// Bit size of the random generator data type
   static auto constexpr bit_size = Nbits;
 
@@ -21,9 +20,9 @@ struct xorshift {
          p. 4 Section "3 Application to Xorshift RNGs" */
       return std::uint64_t { 88172645463325252 };
     } else if constexpr (bit_size == 128) {
-        /// Default value for xor128 from Marsaglia, p. 5 Section "4 Summary"
-      return std::array<std::uint32_t, 4>
-        { 123456789, 362436069, 521288629, 88675123 };
+      /// Default value for xor128 from Marsaglia, p. 5 Section "4 Summary"
+      return std::array<std::uint32_t, 4> { 123456789, 362436069, 521288629,
+                                            88675123 };
     } else
       return nullptr;
   }();
@@ -40,31 +39,25 @@ struct xorshift {
   /// The internal state of the pseudo random generator
   value_type state = initial_state;
 
-
   /// The minimum returned value
   static auto constexpr min() {
     // It cannot return 0
     return std::numeric_limits<result_type>::min() + 1;
   };
 
-
   /// The maximum returned value
   static auto constexpr max() {
     return std::numeric_limits<result_type>::max();
   };
-
 
   /** Initialize the internal state from a user-given value
 
       Do not use the value 0 or the output is always 0
   */
   xorshift(const value_type& s)
-    : state { s }
-  {}
-
+      : state { s } {}
 
   xorshift() = default;
-
 
   /// Compute a new pseudo random integer
   const result_type& operator()() {
@@ -78,28 +71,24 @@ struct xorshift {
       state ^= state >> 7;
       state ^= state << 1;
       state ^= state >> 9;
-    }
-    else if constexpr (bit_size == 64) {
+    } else if constexpr (bit_size == 64) {
       /* The xor64 from Marsaglia, p. 4 Section "3 Application to
          Xorshift RNGs" */
       state ^= state << 13;
       state ^= state >> 7;
       state ^= state << 17;
-    }
-    else if constexpr (bit_size == 128) {
+    } else if constexpr (bit_size == 128) {
       // The xor128 from Marsaglia, p. 5 Section "4 Summary"
-      auto t = state[0]^(state[0]<<11);
+      auto t = state[0] ^ (state[0] << 11);
       state[0] = state[1];
       state[1] = state[2];
       state[2] = state[3];
-      state[3] = state[3]^(state[3]>>19)^(t^(t>>8));
-    }
-    else
+      state[3] = state[3] ^ (state[3] >> 19) ^ (t ^ (t >> 8));
+    } else
       // Just to error in that case
       return nullptr;
 
     return state;
   }
-
 };
 #endif // XORSHIFT_HPP
